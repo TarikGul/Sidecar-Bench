e579acfc6df848df9e39fc36bbed51638e08f463
tarikgul <tarik@parity.io>
2021-03-14 13:49:05 -0400

    update script

--- scripts/aggregate.py
+++ scripts/aggregate.py
@@ -10,7 +10,7 @@ def scan_files(folder: str) -> dict:
         for file in files:
             filename, extension = os.path.splitext(file)
             split_filename = filename.split('-')
-            if extension == '.txt':
+            if extension == '.txt' and filename != 'summary':
                 parse_file(folder, file, data, split_filename[1])
 
     return data
@@ -46,8 +46,8 @@ def parse_file(folder: str, filename: str, data: dict, version: str) -> None:
                 version_data['Completed'].append(split_line[3])
             elif split_line[0] == 'Failed':
                 version_data['Failed'].append(split_line[2])
-            elif split_line[0] == 'Timeouts':
-                version_data['Timeouts:'].append(split_line[1])
+            elif split_line[0] == 'Timeouts:':
+                version_data['Timeouts'].append(split_line[1])
             elif split_line[0] == 'Avg':
                 version_data['Avg-Latency'].append(split_line[2])
             elif split_line[0] == 'Min':
@@ -87,7 +87,7 @@ def performance(data: dict, versions: list) -> None:
 
     for i in range(len(versions)):
         if i == 0:
-            data[versions[i]]['performance'] = 'Earliest version, no data to compare too'
+            data[versions[i]]['Performance'] = 'Earliest version, no data to compare too'
             prev_stable_version = versions[i]
         else:
             cur = data[versions[i]]
@@ -97,17 +97,15 @@ def performance(data: dict, versions: list) -> None:
                 avg_prev_latency = prev['Avg-Latency']
                 cur_average = (float(avg_cur_latency[0][:-2]) + float(avg_cur_latency[1][:-2])) / 2
                 prev_average = (float(avg_prev_latency[0][:-2]) + float(avg_prev_latency[1][:-2])) / 2
-                # Calculate the increase or decrease in performance
+                # Calculate the increase or decrease in Performance
                 diff = cur_average - prev_average
                 perf = round(((diff) / 100.0) * -1.0, 4)
-                data[versions[i]]['performance'] = str(perf) + '%'
+                data[versions[i]]['Performance'] = str(perf) + '%'
                 # Set the new stable version to the current
                 prev_stable_version = versions[i]
             else:
-                data[versions[i]]['performance'] = 'Unstable: No analytics on this version'
+                data[versions[i]]['Performance'] = 'Unstable: No analytics on this version'
                 continue
-        
-        print('version: ', versions[i] + ' ' + data[versions[i]]['performance'])
 
 def is_stable(version_data: dict):
     bench1 = int(version_data['Failed'][0])
@@ -117,8 +115,40 @@ def is_stable(version_data: dict):
     else:
         return True
 
-def write_file():
-    print('do something')
+def write_file(data: dict, versions: list, path: str) -> None:
+    with open(path, 'w') as f:
+        for version in versions:
+            f.write('Sidecar Version: ' + version + '\n')
+            f.write('                  Benchmark-1         Benchmark-2' + '\n')
+            f.write('Requests/sec :      ' + data[version]['Requests/sec'][0]
+                    + space(data[version]['Requests/sec'][0]) + data[version]['Requests/sec'][1] + '\n')
+            f.write('Transfer/sec :      ' + data[version]['Transfer/sec'][0]
+                    + space(data[version]['Transfer/sec'][0]) + data[version]['Transfer/sec'][1] + '\n')
+            f.write('Completed Req:      ' + data[version]['Completed'][0]
+                    + space(data[version]['Completed'][0]) + data[version]['Completed'][1] + '\n')
+            f.write('Timeout Req  :      ' + data[version]['Timeouts'][0] 
+                    + space(data[version]['Timeouts'][0]) + data[version]['Timeouts'][1] + '\n')
+            f.write('Failed Req   :      ' + data[version]['Failed'][0]
+                    + space(data[version]['Failed'][0]) + data[version]['Failed'][1] + '\n')
+            f.write('Avg-Latency  :      ' + data[version]['Avg-Latency'][0]
+                    + space(data[version]['Avg-Latency'][0]) + data[version]['Avg-Latency'][1] + '\n')
+            f.write('Min-Latency  :      ' + data[version]['Min-Latency'][0]
+                    + space(data[version]['Min-Latency'][0]) + data[version]['Min-Latency'][1] + '\n')
+            f.write('Max-Latency  :      ' + data[version]['Max-Latency'][0]
+                    + space(data[version]['Max-Latency'][0]) + data[version]['Max-Latency'][1] + '\n')
+            f.write('Performance  : ' + data[version]['Performance'])
+            f.write('\n')
+            f.write('\n')
+
+        f.truncate()
+
+def space(data1: str) -> str:
+    '''
+    Default amount of space is 18
+    '''
+    space_int = 19 - len(data1) 
+    space_str = space_int * ' '
+    return space_str
 
 def main():
     '''
@@ -127,11 +157,17 @@ def main():
     parser = argparse.ArgumentParser()
 
     '''
-    Data as a dictionary, with necessary key value pairs
+    Data as a dictionary, with necessary key value pairs. Each function that "data"
+    passes through is mutating the structure of this data variable. We are not 
+    cloning the dictionary anywhere.
     '''
     data = scan_files('./benchmarks/gcp-instance/')
 
-    aggregate = aggregate_benchmarks(data)
+    ordered_versions = aggregate_benchmarks(data)
+
+    path_to_file = './benchmarks/gcp-instance/summary.txt';
+    
+    write_file(data, ordered_versions, path_to_file)
 
 if __name__ == "__main__":
     main()
